c92b3a54974f50c9b826b6e63d4d06a3caf519d8
gauravmakkar <gmakkar119@gmail.com>
2017-04-04 13:24:22 +0530

    UI refactoring for User List

--- src/reducers/users/index.js
+++ src/reducers/users/index.js
@@ -6,7 +6,6 @@ import {RECIEVE_USER_LIST} from '../../constants/actionConstants';
 export  function UsersReducer(state={},action){
     switch (action.type) {
         case RECIEVE_USER_LIST:
-            console.log("MIL GAYA BHAI")
             let receivedList = action.data.complete_data;
             return {...state,receivedList}
 
